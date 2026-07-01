760459d2548caa6320187d858d09193162bec433
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-07-01 17:09:49 +0500

    Add the user registration endpoint to this app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 //External modules
-
+import express from 'express'
 //Internal modules
+import pool from './config/database.js';
+const app = express();
+
+pool.connect();
+
+import userRoutes from './modules/user/index.js';
+
+//Middleware to parse JSON requests
+app.use(express.json());    
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded requests
+
+//Define the routes for user-related operations
+app.use('/api/users', userRoutes);
+
+//Export the app instance for use in other modules
+export default app;
