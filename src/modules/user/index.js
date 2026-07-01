f9f8a1fe9de76ba83f12413e2c54c5548e8a3e44
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-07-01 17:01:44 +0500

    This index.js is responsible for giving the routes/ functions and so on which we need outside that moduleso, we can say that this index.js is act like a getway

---
+++ src/modules/user/index.js
@@ -0,0 +1,5 @@
+//Import the userRoutes from the routes/users.routes.js file
+import userRoutes from './routes/users.routes.js';
+
+//export the userRoutes for use in other modules
+export default userRoutes;
